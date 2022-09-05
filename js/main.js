154f1fafbbcd2b0b5fac45ff400df4bce48e42a1
Oleg Khutorianskiy <compilated510@outlook.com>
2022-09-06 01:11:28 +0200

    Animated Header

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ $(function(){
 	});
 
 });
+
+
+let navbar = document.getElementById('navbar').classList
+let active_class = "navbar_scrolled"
+
+window.addEventListener('scroll', e => {
+	if(scrollY > 580) navbar.add(active_class)
+	else navbar.remove(active_class)
+  })
